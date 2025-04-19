f1c0f3c4115a7c3878c83a4feaf87020bf6f3a0e
Misheck0 <tellemmisheck@gmail.com>
2025-04-19 13:36:18 +0200

    ds

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-// server.js
 import express from 'express';
 import trackHandler from './track.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Add root route
+app.get('/', (req, res) => {
+  res.send('FY Cargo Tracker API is running');
+});
+
+// Add your tracking route
 app.get('/api/track', trackHandler);
 
 app.listen(port, () => {
